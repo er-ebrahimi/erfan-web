a55c1a3cae29b188060efe89c5d8c0ea4636d80e
Erfan Ebrahimi <erfan.es1381@gmail.com>
2026-02-11 23:51:02 +0100

    fix: update middleware configuration for internationalized pathnames and add robots.txt file

--- next/middleware.ts
+++ next/middleware.ts
@@ -4,7 +4,9 @@ import { routing } from './i18n/routing';
 
 export default createMiddleware(routing);
 
+
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(fa|fr|en)/:path*'],
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)",
+  ],
 };
